7eff94625fc8e076d5b47448082fc5b720dd16ed
bora <nhatbaoxxd@gmail.com>
2024-03-20 01:13:38 +0700

    Using local fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className='text-4xl font-thin'>Xin chào các bạn</h1>
+      <h1 className="text-5xl font-bold">Xin chào các bạn</h1>
+      <p className="text-2xl font-semibold">Đây là trang chủ của tôi</p>
+      <p className="font-roboto text-xl font-medium">Đây là Next.js</p>
     </div>
   )
 }
