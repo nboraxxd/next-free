b176ca5f36151c8d65491ec9a6ef56e102ba8aff
bora <nhatbaoxxd@gmail.com>
2024-03-19 22:18:25 +0700

    Update font in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Input } from '@/components/ui/input'
-
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
-    <div className='flex w-full max-w-sm items-center space-x-2'>
-      <Input type='email' placeholder='Email' />
-      <Button type='submit'>Subscribe</Button>
+    <div>
+      <h1 className='text-4xl font-thin'>Xin chào các bạn</h1>
     </div>
   )
 }
